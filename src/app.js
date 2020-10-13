c48b12a0bd20f83eab806217074f89f442668ef2
Noorul Huda <mdnoorulhuda045@gmail.com>
2020-10-13 14:33:07 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forcast = require('./utils/forcast')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 
 // Define paths for express config
@@ -73,6 +74,6 @@ app.get('*', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port, 3000')
+app.listen(port, () => {
+    console.log('Server is up on port, '+ port)
 })
